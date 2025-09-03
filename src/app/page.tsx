076fbfdaad4d44ded82be820bf1a3f3530d78167
Justin Kian Romeon <romeonjustin@gmail.com>
2025-09-03 14:57:51 +0800

    feat: integrate shadcn/ui and add dark mode support

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import ThemeToggle from '@/components/ThemeToggle'
+import { Button } from '@/components/ui/button'
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
 import React from 'react'
 
@@ -5,17 +7,22 @@ function HomePage() {
   return (
     <div>
       <SignedOut>
-        <SignInButton mode='modal' />
-        <SignUpButton mode='modal'>
-          <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+        <SignInButton mode='modal'>
+          <Button variant='default'>
             Sign Up
-          </button>
+          </Button>
+        </SignInButton>
+        <SignUpButton mode='modal'>
+          <Button variant='secondary'>
+            Sign Up
+          </Button>
         </SignUpButton>
       </SignedOut>
       <SignedIn>
         <UserButton />
       </SignedIn>
-    </div>
+      <ThemeToggle />
+    </div >
   )
 }
 
